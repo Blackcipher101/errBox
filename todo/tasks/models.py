dc1d0f63215e770234cb8350d9b76f7df3fdc965
Nehal Nevle <nehalsuperdude@gmail.com>
2020-08-14 03:11:37 +0530

    added migratation error and template error

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -17,7 +17,7 @@ class Username(models.Model):
 
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.ForeignKey(Username, on_delete=models.CASCADE)
+    username = models.Foreignkey(Username, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000, blank=True)
     date_of_creation = models.DateTimeField(auto_now_add=True)
